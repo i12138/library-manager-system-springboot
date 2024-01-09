aa529279c0f9471fc22c892d23621450bdaa3f1f
ddk <1571905344@qq.com>
2024-01-10 01:28:12 +0800

    Initial commit

---
+++ src/main/java/com/zbw/LibraryManagerSystemSpringbootApplication.java
@@ -0,0 +1,17 @@
+package com.zbw;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@MapperScan("com.zbw.mapper")
+@ServletComponentScan
+public class LibraryManagerSystemSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagerSystemSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zbw/LibraryManagerSystemSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerSystemSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
